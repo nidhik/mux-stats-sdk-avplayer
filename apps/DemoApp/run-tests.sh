c9a58133cb07c3ebf95fbe28c3b0ff2a8f8afe0c
Nidhi Kulkarni <nkulkarni@mux.com>
2022-01-19 23:51:14 -0500

    ui test sim

--- apps/DemoApp/run-tests.sh
+++ apps/DemoApp/run-tests.sh
@@ -12,5 +12,5 @@ cd apps/DemoApp
 pod deintegrate && pod update
 xcodebuild -workspace DemoApp.xcworkspace \
            -scheme "DemoApp" \
-           -destination 'platform=iOS Simulator,name=iPhone 11,OS=14.1' \
+           -destination 'platform=iOS Simulator,name=iPhone 11,OS=15.2' \
            test
